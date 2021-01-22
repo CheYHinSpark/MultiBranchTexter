96e1ae3ea41399279aab7cfa1dbd828a2312459e
CheYHinSpark <465126135@qq.com>
2021-01-23 00:49:27 +0800

    修正了一点资源

--- obj/Debug/netcoreapp3.1/Controls/ConnectingLine.g.i.cs
+++ obj/Debug/netcoreapp3.1/Controls/ConnectingLine.g.i.cs
@@ -60,7 +60,7 @@ namespace MultiBranchTexter {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MultiBranchTexter;V1.0.0.0;component/controls/connectingline.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MultiBranchTexter;component/controls/connectingline.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\ConnectingLine.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
