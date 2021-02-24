17b09f0b3dd558205da77938135389ce3d2a82b4
CheYHinSpark <465126135@qq.com>
2021-02-24 18:10:24 +0800

    解决了最大化窗口的问题

--- Controls/Window/MetroWindow.cs
+++ Controls/Window/MetroWindow.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
 using MultiBranchTexter.ViewModel;
+using System.Windows.Interop;
 
 namespace MultiBranchTexter
 {
@@ -32,6 +34,112 @@ namespace MultiBranchTexter
         }
         #endregion
 
+        #region 我也不懂这一坨东西是什么，但是它可以起到不遮盖任务栏的作用
+        void Win_SourceInitialized(object sender, System.EventArgs e)
+        {
+            var handle = new WindowInteropHelper(this).Handle;
+            var handleSource = HwndSource.FromHwnd(handle);
+            if (handleSource == null)
+                return;
+            handleSource.AddHook(WindowProc);
+            this.SourceInitialized -= Win_SourceInitialized;
+        }
+
+        private static System.IntPtr WindowProc(
+           System.IntPtr hwnd,
+           int msg,
+           System.IntPtr wParam,
+           System.IntPtr lParam,
+           ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = false;// <--让最小高宽生效
+                    break;
+            }
+            return (System.IntPtr)0;
+        }
+
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            // Adjust the maximized size and position to fit the work area of the correct monitor
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            System.IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != System.IntPtr.Zero)
+            {
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top) - 1;
+                //不知道为什么必须要有一个做出一点偏差才会生效
+            }
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+
+            public RECT rcMonitor = new RECT();
+
+            public RECT rcWork = new RECT();
+
+            public int dwFlags;
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            /// <summary>x coordinate of point.</summary>
+            public int x;
+            /// <summary>y coordinate of point.</summary>
+            public int y;
+
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public static readonly RECT Empty = new RECT();
+        }
+        #endregion
+
         //标题栏
         private CheckBox MaxButton;
         private TextBlock WindowTitleTbl;
@@ -43,8 +151,7 @@ namespace MultiBranchTexter
         public MetroWindow()
         {
             this.Loaded += MetroWindow_Loaded;
-            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+            this.SourceInitialized += Win_SourceInitialized;
         }
 
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
@@ -94,14 +201,20 @@ namespace MultiBranchTexter
                 {
                     (Application.Current.MainWindow as MainWindow)
                         .ViewModel.SaveFile((Application.Current.MainWindow as MainWindow).GetFCC());
+                    WindowStyle = WindowStyle.SingleBorderWindow;
                     Close();
                 }
                 else if (warnResult == MessageBoxResult.No)
                 {
+                    WindowStyle = WindowStyle.SingleBorderWindow;
                     Close();
                 }
             }
-            else { Close(); }
+            else
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow;
+                Close();
+            }
         }
 
         private void MaxButton_Click(object sender, RoutedEventArgs e)
